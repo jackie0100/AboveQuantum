b407009461461d3f203f1c327584f2f8a443920f
Jackie Engberg Christensen <jackie.engberg@gmail.com>
2017-11-18 02:43:12 +0100

    no message

---
+++ Assets/Characters/OverviewCameraControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverviewCameraControl : MonoBehaviour
+{
+    [SerializeField]
+    Camera overViewCamera;
+
+    [SerializeField]
+    Camera playerCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            overViewCamera.cullingMask ^= 1 << 9;
+            overViewCamera.cullingMask ^= 1 << 8;
+        }
+    }
+}
