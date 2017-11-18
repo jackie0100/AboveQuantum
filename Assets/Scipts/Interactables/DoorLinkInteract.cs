ad55e696196e79d6a5fcf6ef29c69429214b1251
Jackie Engberg Christensen <jackie.engberg@gmail.com>
2017-11-18 22:45:33 +0100

    no message

---
+++ Assets/Scipts/Interactables/DoorLinkInteract.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLinkInteract : QuantumLink
+{
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    float rotationLimit;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+    }
+
+    public override void ExecuteLocalQuantumAction()
+    {
+        base.ExecuteLocalQuantumAction();
+
+        StartCoroutine(RotateDoor());
+    }
+
+    public IEnumerator RotateDoor()
+    {
+        while (this.transform.eulerAngles.y >= rotationLimit)
+        {
+            this.transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return null;
+    }
+}
