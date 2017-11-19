da4234331daa96f58f3f19183bd40cfd4af31a08
Jackie Engberg Christensen <jackie.engberg@gmail.com>
2017-11-19 05:39:18 +0100

    no message

--- Assets/Scipts/Interactables/KeyInteractable.cs
+++ Assets/Scipts/Interactables/KeyInteractable.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyInteractable : MonoBehaviour {
-
+public class KeyInteractable : MonoBehaviour
+{
 	// Use this for initialization
 	void Start () {
 		
