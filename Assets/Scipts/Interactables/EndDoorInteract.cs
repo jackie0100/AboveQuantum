035f74bd91ce9fd4ebc2b6592e304f944d50f2eb
Jackie Engberg Christensen <jackie.engberg@gmail.com>
2017-11-19 08:36:42 +0100

    Fixed Enddoor

---
+++ Assets/Scipts/Interactables/EndDoorInteract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndDoorInteract : Interactable
+{
+    [SerializeField]
+    string nextLevelName;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Interact()
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
